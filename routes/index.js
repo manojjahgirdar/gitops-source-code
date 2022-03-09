c044f5a462350f8a2e65e24967f48515cb1096da
Manoj Jahgirdar <52746337+manojjahgirdar@users.noreply.github.com>
2022-03-09 15:57:47 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Automate OpenShift Deployments with Tekton CI and Argo CD.. We Did it! Finally' });
+  res.render('index', { title: 'Automate OpenShift Deployments with Tekton CI and Argo CD.. We Did it! Finally!' });
 });
 
 module.exports = router;
